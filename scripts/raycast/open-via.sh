4fed3b9d11c2c893aea02b433385789369e9057f
masuda.sota <masuda.sota@moneyforward.co.jp>
2024-11-24 23:41:26 +0900

    add raycast script / add keychrone via setting file

---
+++ scripts/raycast/open-via.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Site: via.app - keyboard setting
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Open VIA site
+# @raycast.author Sota
+# @raycast.authorURL https://raycast.com/Sota
+
+# This is keyboard setting site for QMK firmware
+# e.g. keychron series keyboard
+URL="https://usevia.app/"
+# Open the URL via the default browser
+open "$URL"
+
+
