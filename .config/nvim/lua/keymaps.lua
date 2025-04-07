32211ef223d62345cf4c59bf2098cac260f36ef5
masuda.sota <masuda.sota@moneyforward.co.jp>
2025-04-07 12:54:35 +0900

    update find files in nvim

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -9,6 +9,16 @@ vim.keymap.set("n", "<leader>q", ":q<CR>", { noremap = true, silent = true })  -
 vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
 -- Telescope検索
+-- * search files including hidden files
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", function()
+    builtin.find_files({
+        hidden = true,
+        file_ignore_patterns = { ".git/", "node_modules/", ".cache/", ".DS_Store" },
+    })
+end, { noremap = true, silent = true })
+
 vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>", { noremap = true, silent = true })
+-- * grep search
 vim.keymap.set("n", "<C-f>", ":Telescope live_grep<CR>", { noremap = true, silent = true })
 
